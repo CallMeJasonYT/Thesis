b4ea0977725ce3e93d8556a6d4c1dfdf565e3514
Jason Pavlopoulos <Jasonpavlop1@gmail.com>
2024-12-08 16:17:33 +0200

    Update App.tsx

--- teacher-panel/src/App.tsx
+++ teacher-panel/src/App.tsx
@@ -3,7 +3,7 @@ import WebSocketManager from './WebSocketManager';
 import WebSocketComponent from './WebSocketComponent';
 import PlayerActivity from './PlayerActivity';
 
-const wsManager = new WebSocketManager('ws://localhost:8080'); // Central WebSocketManager instance
+const wsManager = new WebSocketManager('ws://localhost:8080');
 
 const App = () => (
     <div>
